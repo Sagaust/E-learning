43c2634acee749f5ea71cac20e246e31e36021c8
Sagaust <austinorge@yahoo.com>
2023-10-13 03:53:55 +0100

    Update CoursePage.jsx
    add an api link to connect with django backend

--- src/components/courses/CoursePage.jsx
+++ src/components/courses/CoursePage.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../components/common/Navbar';
 import Footer from '../components/common/Footer';
 
 function CoursePage() {
+    const [course, setCourse] = useState({ title: '', description: '' });
+
+    useEffect(() => {
+        // Replace with your Django API endpoint
+        const apiUrl = 'https://www.africanphilosophers.online/api/courses/list/';
+
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                setCourse({
+                    title: data.title,
+                    description: data.description
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching course data:', error);
+            });
+    }, []); // Empty dependency array means this useEffect runs once when the component mounts
+
     return (
         <div>
             <Navbar />
             
             <section className="course-detail">
-                <h1>Course Title</h1>
-                <p>Course Description</p>
+                <h1>{course.title}</h1>
+                <p>{course.description}</p>
                 {/* More details, course content, instructor info, etc. */}
             </section>
 
